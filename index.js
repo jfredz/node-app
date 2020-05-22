45e31db090efe1f3e216cc2771996a35411ae0b0
Malai Cristian <chris@Malais-MacBook-Pro.local>
2020-05-22 22:20:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const { readFile } = require('fs').promises
+const app = express()
+
+app.get('/', async (request, response) => {
+    response.send(await readFile('./home.html', 'utf8'))
+})
+
+app.listen(process.env.PORT || 3000, () => console.log('app started boss.'))
